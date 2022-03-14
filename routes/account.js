5928c13282bea93e95fd8adf36e7bfc1ae905c8d
Nithaparan Francis <nithaparanf@gmail.com>
2022-03-13 22:14:06 -0400

    image url added to models and update api for both franchise and storeowner added

--- routes/account.js
+++ routes/account.js
@@ -8,8 +8,9 @@ router.post('/createAccount', accountController.createAccount);
 router.post('/appleSignIn',accountController.appleSignIn)
 router.get('/getProfile/:accountId', accountController.getProfile);
 router.post('/franchiseSignup', accountController.franchiseSignup);
+router.put('/franchiseUpdate', accountController.franchiseUpdate);
 router.post('/storeOwenerSignup', accountController.storeOwenerSignup);
+router.put('/storeOwenerUpdate', accountController.storeOwnerUpdate);
 router.get('/getAllFranchises', accountController.getAllFranchises);
 router.get('/getAllStoreOwners', accountController.getAllStoreOwners);
-
 module.exports = router
--- services/accountService.js
+++ services/accountService.js
@@ -3,7 +3,8 @@ const FranchiseCategory = require("../models/franchiseCategory");
 var Account = require("../models/account");
 const Response = require("../models/response");
 const jwt = require("jsonwebtoken");
-
+const Franchise = require("../models/franchise");
+const Storeowner = require("../models/storeowner");
 const jwksClient = require("jwks-rsa");
 
 const client = jwksClient({
@@ -124,6 +125,9 @@ exports.signUpApple = async (
 exports.getFranchiseCategories = (franchiseCategories) => {
   var ObjectId = require("mongoose").Types.ObjectId;
   var objIds = [];
+  if (franchiseCategories == null) {
+    return objIds
+  }
   franchiseCategories.forEach((element) => {
     objIds.push(new ObjectId(element));
   });
@@ -143,14 +147,7 @@ exports.saveAccount = async (newAccount, res) => {
   return;
 };
 
-exports.saveStoreOwner = async (newStoreOwener, res) => {
-  try {
-    await newStoreOwener.save();
-  } catch (error) {
-    return error;
-  }
-  return;
-};
+
 
 exports.validateAppleSignIn = async (token) => {
   const json = jwt.decode(token, { complete: true });
@@ -192,4 +189,93 @@ exports.saveFranchise = async (newfranchise, res) => {
     return error;
   }
   return;
+};
+
+exports.saveStoreowner = async (newStoreOwener, res) => {
+  try {
+    await newStoreOwener.save();
+  } catch (error) {
+    return error;
+  }
+  return;
+};
+
+exports.updateFranchise = async (accountWithID,minimumDeposit,franchiseName,franchiseCategories,countryCode,phoneNumber,imageUrl,res,next) => {
+  const franchiseCategoryObjects =
+  exports.getFranchiseCategories(franchiseCategories);
+  await Franchise.findOne({ _id: accountWithID.franchise }).exec(function (
+    err,
+    franchiseWithID
+  ) {
+    if (franchiseWithID) {
+      franchiseWithID.minimumDeposit = minimumDeposit != null ? minimumDeposit : franchiseWithID.minimumDeposit
+      franchiseWithID.franchiseName = franchiseName != null ? franchiseName : franchiseWithID.franchiseName
+      franchiseWithID.franchiseCategories = franchiseCategories != null ? franchiseCategoryObjects : franchiseWithID.franchiseCategories
+      franchiseWithID.countryCode = countryCode != null ? countryCode : franchiseWithID.countryCode
+      franchiseWithID.phoneNumber = phoneNumber != null ? phoneNumber : franchiseWithID.phoneNumber
+      franchiseWithID.imageUrl = imageUrl != null ? imageUrl : franchiseWithID.imageUrl
+      try {
+        exports.saveFranchise(franchiseWithID, res).then((err) => {
+          if (err) {
+            const error1 = new Error();
+            error1.message = "Saving failed";
+            error1.code = 5000;
+            error1.error = error
+            error1.status = 500;
+            console.log(error1)
+            return next(error1);
+          } else {
+            accountWithID.franchise = franchiseWithID
+            res.status(200).json(new Response(2000, "", accountWithID));
+          }
+        });
+      } catch (error) {
+        next(error);
+        return;
+      }
+    } else {
+    }
+   } );   
+};
+
+exports.updateStoreowner = async (accountWithID,apartmentNumber,addressLine1,addressLine2,city,province,postalCode,pictures,countryCode,phoneNumber,imageUrl,res,next) => {
+  
+  await Storeowner.findOne({ _id: accountWithID.storeOwner }).exec(function (
+    err,
+    storeOwnerWithID
+  ) {
+    if (storeOwnerWithID) {
+      storeOwnerWithID.apartmentNumber = apartmentNumber != null ? apartmentNumber : storeOwnerWithID.apartmentNumber
+      storeOwnerWithID.addressLine1 = addressLine1 != null ? addressLine1 : storeOwnerWithID.addressLine1
+      storeOwnerWithID.addressLine2 = addressLine2 != null ? addressLine2 : storeOwnerWithID.addressLine2
+      storeOwnerWithID.city = city != null ? city : storeOwnerWithID.city
+      storeOwnerWithID.province = province != null ? province : storeOwnerWithID.province
+      storeOwnerWithID.postalCode = postalCode != null ? postalCode : storeOwnerWithID.postalCode
+      storeOwnerWithID.pictures = pictures != null ? pictures : storeOwnerWithID.pictures
+      storeOwnerWithID.countryCode = countryCode != null ? countryCode : storeOwnerWithID.countryCode
+      storeOwnerWithID.phoneNumber = phoneNumber != null ? phoneNumber : storeOwnerWithID.phoneNumber
+      storeOwnerWithID.imageUrl = imageUrl != null ? imageUrl : storeOwnerWithID.imageUrl
+      try {
+        exports.saveStoreowner(storeOwnerWithID, res).then((err) => {
+          if (err) {
+            const error1 = new Error();
+            error1.message = "Saving failed";
+            error1.code = 5000;
+            error1.error = error
+            error1.status = 500;
+            console.log(error1)
+            return next(error1);
+          } else {
+            accountWithID.storeOwner = storeOwnerWithID
+            res.status(200).json(new Response(2000, "", accountWithID));
+          }
+        });
+      } catch (error) {
+        console.log(error)
+        next(error);
+        return;
+      }
+    } else {
+    }
+   } );   
 };
